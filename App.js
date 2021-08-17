25cf9986ab62012d35d6f7d71226814b9a0743ad
Chung10Kr <coohg93@hanmail.net>
2021-08-18 00:39:56 +0900

    - List, Music delete - AppJs update from opens

--- App.js
+++ App.js
@@ -1,122 +1,227 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, TouchableOpacity, View, Image, Text } from 'react-native'
+import { Audio } from 'expo-av'
+import Icon from 'react-native-vector-icons/AntDesign';
 import Loading from "./view/Loading"
-import List from "./view/List"
-import { Audio } from 'expo-av';
 
-import * as SQLite from 'expo-sqlite';
+const audioBookPlaylist = [
+	{
+		title: 'Hamlet - Act I',
+		author: 'William Shakespeare',
+		source: 'Librivox',
+		uri:
+			'https://ia800204.us.archive.org/11/items/hamlet_0911_librivox/hamlet_act1_shakespeare.mp3',
+		imageSource: 'http://ia800209.us.archive.org/9/items/LibrivoxCdCoverArt8/Oko_no_Hosomichi_1104.jpg'
+	},
+	{
+		title: 'Hamlet - Act II',
+		author: 'William Shakespeare',
+		source: 'Librivox',
+		uri:
+			'https://ia600204.us.archive.org/11/items/hamlet_0911_librivox/hamlet_act2_shakespeare.mp3',
+		imageSource: 'http://www.archive.org/download/LibrivoxCdCoverArt8/hamlet_1104.jpg'
+	},
+	{
+		title: 'Hamlet - Act III',
+		author: 'William Shakespeare',
+		source: 'Librivox',
+		uri: 'http://www.archive.org/download/hamlet_0911_librivox/hamlet_act3_shakespeare.mp3',
+		imageSource: 'http://ia800209.us.archive.org/9/items/LibrivoxCdCoverArt8/North_Boston_1104.jpg'
+	},
+	{
+		title: 'Hamlet - Act IV',
+		author: 'William Shakespeare',
+		source: 'Librivox',
+		uri:
+			'https://ia800204.us.archive.org/11/items/hamlet_0911_librivox/hamlet_act4_shakespeare.mp3',
+		imageSource: 'http://www.archive.org/download/LibrivoxCdCoverArt8/hamlet_1104.jpg'
+	},
+	{
+		title: 'Hamlet - Act V',
+		author: 'William Shakespeare',
+		source: 'Librivox',
+		uri:
+			'https://ia600204.us.archive.org/11/items/hamlet_0911_librivox/hamlet_act5_shakespeare.mp3',
+		imageSource: ''
+	}
+]
 
-let playObject = new Audio.Sound();
-const db = SQLite.openDatabase('db.childApp') 
-let tableNm = "test1";
-let musicType = [ "자연" , "동물" , "역할" ]
-let musicList=[
-  {id:1 , type:musicType[0] ,  musicTitle : "풍선아 풍선아" ,musicFile : "toBalloons.mp3",favYn:false},
-  {id:2 , type:musicType[0] ,  musicTitle : "SHE" ,musicFile : "she.mp3",favYn:false},
-  {id:3 , type:musicType[1] ,  musicTitle : "SHE1" ,musicFile : "she.mp3",favYn:false},
-  {id:4 , type:musicType[1] ,  musicTitle : "SHE1" ,musicFile : "she.mp3",favYn:false},
-  {id:5 , type:musicType[1] ,  musicTitle : "SHE1" ,musicFile : "she.mp3",favYn:false},
-  {id:6 , type:musicType[1] ,  musicTitle : "SHE1" ,musicFile : "she.mp3",favYn:false},
-  {id:7 , type:musicType[1] ,  musicTitle : "SHE1" ,musicFile : "she.mp3",favYn:false},
-  {id:8 , type:musicType[1] ,  musicTitle : "SHE1" ,musicFile : "she.mp3",favYn:false},
-  {id:9 , type:musicType[1] ,  musicTitle : "SHE1" ,musicFile : "she.mp3",favYn:false},
-  {id:10 , type:musicType[2] ,  musicTitle : "SHE2" ,musicFile : "she.mp3",favYn:false},
-  {id:11 , type:musicType[2] ,  musicTitle : "SHE2" ,musicFile : "she.mp3",favYn:false},
-];
-const sound = new Audio.Sound();
-export default class extends React.Component {
-  constructor (props){
-    super(props)
-  };
-  state = {
-    isLoading: true ,
-    musicList:[]
-  };
-  componentWillUnmount(){
-    this.createTable();
-  }
-  componentDidMount() {
-    this.getMusicList();
-  };
-  // 테이블 생성
-  createTable = async () =>{
-    db.transaction(tx => {
-      tx.executeSql(
-        'CREATE TABLE IF NOT EXISTS '+tableNm+' (id INTEGER)'
-      );
-    })
-  };
-  async insertFav(id){
-    db.transaction(tx => {tx.executeSql('INSERT INTO '+tableNm+' (ID) values (?)', [id]);},);
-  };
-  async deleteFav(id){
-    db.transaction(tx => {tx.executeSql('DELETE FROM '+tableNm+' WHERE ID = ? ', [id]);},);
-  };
-  selectFav(){
-    return new Promise((resolve=>{
-      db.transaction(
-        tx => {
-          tx.executeSql('SELECT ID FROM '+tableNm+'', [], (trans, result) => {
-            let row = result.rows;
-            resolve(row)
-            return row
-          });
-        }
-      );
-    }))
-  }
-  _onMusicFavSelected = (flag , id ) => {
-    /**
-     * false -> 즐겨찾기 안함
-     * true -> 즐겨찾기 함
-    */
-    !flag ? this.insertFav(id) : this.deleteFav(id);
-    this.getMusicList();
-  };
-  
-  onMusicTurn = async (id)=>{
+export default class App extends React.Component {
+	state = {
+		isLoading: false,
+		isPlaying: false,
+		playbackInstance: null,
+		currentIndex: 0,
+		volume: 1.0,
+		isBuffering: true
+	}
 
-    const musicitem = musicList.find(function(music){
-      if( music.id == id ){
-        return music;
-      }
-    });
-    
-    let musicPath =  `http://172.30.1.14:3000/public/${musicitem.musicFile}` ;
+	async componentDidMount() {
+		try {
+			await Audio.setAudioModeAsync({
+				allowsRecordingIOS: false,
+				interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX,
+				playsInSilentModeIOS: true,
+				interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DO_NOT_MIX,
+				shouldDuckAndroid: true,
+				staysActiveInBackground: true,
+				playThroughEarpieceAndroid: true
+			})
 
-    await sound.unloadAsync();
-    await sound.loadAsync(  { uri:  musicPath } );
-    await sound.playAsync();
-  };
-  onStopMusic = async()=> {
-    await sound.unloadAsync();
-  }
-  getMusicList = async () => {
-    const objectFav = await this.selectFav();
-    let favList = [];
-    for( var i=0 ; i <objectFav["_array"].length ; i++ ){
-      favList.push( objectFav["_array"][i]["id"] )
-    };
-    musicList = musicList.map(function(music){
-      if( favList.indexOf( music.id ) >= 0 ){
-        music.favYn = true;
-      }else{
-        music.favYn = false;
-      };
-      return music;
-    });
+			this.loadAudio()
+		} catch (e) {
+			console.log(e)
+		}
+	}
 
-    this.setState({isLoading:false , musicList:musicList })
-  };
-  render() {
-    
-    let { isLoading } = this.state;
-    
-    return isLoading ? (
-      <Loading />
-    ) : (
-      <List _musicList={musicList} _onMusicFavSelected={this._onMusicFavSelected} _onMusicTurn={this.onMusicTurn} _onStopMusic={this.onStopMusic} />
-    );
-  }
+	async loadAudio() {
+		const { currentIndex, isPlaying, volume } = this.state
+
+		try {
+			const playbackInstance = new Audio.Sound()
+			const source = {
+				uri: audioBookPlaylist[currentIndex].uri
+			}
+
+			const status = {
+				shouldPlay: isPlaying,
+				volume: volume
+			}
+
+			playbackInstance.setOnPlaybackStatusUpdate(this.onPlaybackStatusUpdate)
+			await playbackInstance.loadAsync(source, status, false)
+			this.setState({
+				playbackInstance
+			})
+		} catch (e) {
+			console.log(e)
+		}
+	}
+
+	onPlaybackStatusUpdate = status => {
+		this.setState({
+			isBuffering: status.isBuffering
+		})
+	}
+
+	handlePlayPause = async () => {
+		const { isPlaying, playbackInstance } = this.state
+		isPlaying ? await playbackInstance.pauseAsync() : await playbackInstance.playAsync()
+
+		this.setState({
+			isPlaying: !isPlaying
+		})
+	}
+
+	handlePreviousTrack = async () => {
+		let { playbackInstance, currentIndex } = this.state
+		if (playbackInstance) {
+			await playbackInstance.unloadAsync()
+			this.setState({
+				currentIndex : (currentIndex === 0 ? audioBookPlaylist.length -1 : currentIndex-1)
+			});
+			this.loadAudio()
+		}
+	}
+
+	handleNextTrack = async () => {
+		let { playbackInstance, currentIndex } = this.state
+		if (playbackInstance) {
+			await playbackInstance.unloadAsync()
+			this.setState({
+				currentIndex: (currentIndex+1 > audioBookPlaylist.length - 1 ? 0 : currentIndex+1)
+			});
+			this.loadAudio()
+		}
+	}
+	renderFileImg() {
+		const { playbackInstance, currentIndex } = this.state
+		return playbackInstance ? (
+			<View style={styles.trackInfo}>
+				<Image
+				style={styles.albumCover}
+				source={{ uri: audioBookPlaylist[currentIndex].imageSource }}
+				/>
+			</View>
+				
+		) : null
+	}
+	renderFileInfo() {
+		const { playbackInstance, currentIndex } = this.state
+		return playbackInstance ? (
+			<View style={styles.trackInfo}>
+				<Text style={[styles.trackInfoText, styles.largeText]}>
+					{audioBookPlaylist[currentIndex].title}
+				</Text>
+				<Text style={[styles.trackInfoText, styles.smallText]}>
+					{audioBookPlaylist[currentIndex].author}
+				</Text>
+				<Text style={[styles.trackInfoText, styles.smallText]}>
+					{audioBookPlaylist[currentIndex].source}
+				</Text>
+			</View>
+		) : null
+	}
+
+	render() {
+		let {isLoading} = this.state;
+
+		return (
+			<View style={styles.container}>
+				{this.renderFileImg()}
+				<View style={styles.controls}>
+					<TouchableOpacity style={styles.control} onPress={this.handlePreviousTrack}>
+						<Icon name="banckward" size={48} /> 
+					</TouchableOpacity>
+					<TouchableOpacity style={styles.control} onPress={this.handlePlayPause}>
+						{this.state.isPlaying ? (
+							<Icon name='pausecircle' size={48} color='#444' />
+						) : (
+							<Icon name='play' size={48} color='#444' />
+						)}
+					</TouchableOpacity>
+					<TouchableOpacity style={styles.control} onPress={this.handleNextTrack}>
+						<Icon name="forward" size={48} /> 
+					</TouchableOpacity>
+				</View>
+				{this.renderFileInfo()}
+			</View>
+		)
+		
+	}
 }
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center'
+	},
+	albumCover: {
+		width: 250,
+		height: 250
+	},
+	trackInfo: {
+		padding: 40,
+		backgroundColor: '#fff'
+	},
+
+	trackInfoText: {
+		textAlign: 'center',
+		flexWrap: 'wrap',
+		color: '#550088'
+	},
+	largeText: {
+		fontSize: 22
+	},
+	smallText: {
+		fontSize: 16
+	},
+	control: {
+		margin: 20
+	},
+	controls: {
+		flexDirection: 'row'
+	}
+})
